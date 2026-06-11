cba82c15f97e7203fc909df2e9aa474e3d758a5d
Amando Luiz Da Cruz <amando.luiz@alunos.ifsuldeminas.edu.br>
2026-06-11 20:39:28 -0300

    Implementa CRUD de obras

--- biblioteca/models.py
+++ biblioteca/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Obra(models.Model):
+    titulo = models.CharField(max_length=200)
+    autor = models.CharField(max_length=200)
+    isbn = models.CharField(max_length=20, unique=True)
+    editora = models.CharField(max_length=150)
+    ano_publicacao = models.PositiveIntegerField()
+    categoria = models.CharField(max_length=100)
+    quantidade = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return self.titulo
