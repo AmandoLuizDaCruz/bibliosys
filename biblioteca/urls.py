dd93a75924758f175aea9f1c2926a978626e82c0
Amando Luiz da Cruz <amandoluizdacruz@hotmail.com>
2026-06-11 20:39:28 -0300

    Implementa CRUD de obras

--- biblioteca/urls.py
+++ biblioteca/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
+
 from . import views
 
+
 urlpatterns = [
-    path('', views.home, name='home'),
+    path("", views.home, name="home"),
+    path("obras/", views.listar_obras, name="listar_obras"),
+    path("obras/cadastrar/", views.cadastrar_obra, name="cadastrar_obra"),
+    path("obras/<int:obra_id>/editar/", views.editar_obra, name="editar_obra"),
+    path("obras/<int:obra_id>/excluir/", views.excluir_obra, name="excluir_obra"),
 ]
